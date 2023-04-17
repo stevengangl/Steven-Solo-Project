14cb161b31cfa2ded3422bce3737b82af54e0690
Steven Gangl <steven.h.gangl@gmail.com>
2023-04-17 11:42:50 -0500

    installed material ui library

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,9 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Title Of App</h2>
+        <h2 className="nav-title">what page were on...Home Page</h2>
+
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
