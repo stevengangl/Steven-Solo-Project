f48628aa365ee7a1c9b05d33bed89e915163efb1
Steven Gangl <steven.h.gangl@gmail.com>
2023-04-24 14:46:48 -0500

    fixed trash spot on card

--- src/components/SimulatorPage/SimulatorTable.jsx
+++ src/components/SimulatorPage/SimulatorTable.jsx
@@ -43,6 +43,7 @@ function SimulatorTable() {
 
     const [expanded, setExpanded] = React.useState(false);
 
+
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
@@ -60,7 +61,7 @@ function SimulatorTable() {
 
     function Calculate() {
         let calories; // shared variable
-        console.log('sim', sim[0].todo)
+        // console.log('sim', sim[0].todo)
 
         if (info.length) {
             if (info[0].gender === 'Male') {
@@ -83,36 +84,12 @@ function SimulatorTable() {
         }
 
 
-
         // this gets the data and retunes with a 1 second delay which fixes timing issue
         setTimeout(() => {
             console.log('Calories:', Math.round(calories));
             return Math.round(calories);
         }, 1000);
     }
-
-    function whatToDo() {
-        let objective = 'hi'; // default value
-        
-        sim.map(item => {
-            if (item.todo == 'Gain') {
-                objective = 'Surplus'; // update the value
-                console.log('objective:', objective);
-            } else {
-                objective = 'deficit'; // update the value for the else block
-                console.log('ELSE objective:', objective);
-            }
-            console.log('ELSE:', objective);
-        });
-        
-        return objective;
-    }
-    
-
-
-
-
-
 
 
     if (!sim || !sim.length) {
@@ -122,40 +99,46 @@ function SimulatorTable() {
 
     return (
         <>
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
 
                 {sim.map((item, index) => (
                     <Card key={index} sx={{
-                        maxWidth: 500, maxHeight: 1500,
+                        maxWidth: 400, maxHeight: 1500, backgroundColor: 'lightblue', border: '1px solid #000',
+                        padding: '25px', margin: '2px'
                     }}>
                         <CardHeader
 
                             title={'Goal: ' + item.todo + ' ' + item.weight + 'lbs'}
-                            subheader={'daily calories ' + Calculate()}
+                            subheader={user.username + ' daily calories ' + Calculate()}
                         />
                         <CardContent>
-                            <Typography variant="body2" color="text.secondary">
-
-                                Average weight {item.todo} (based on 500 {(item.todo === 'Gain' ? 'Surplus' : 'Defecit')} )
+                            <Typography variant="body2" color="" fontSize='18px'>
+                                Approach: light
+                                <br />
+                                Days: {item.weight * 3500 / 250}
+                                <br />
+                                Calorie: {(item.todo === 'Gain' ? 'Surplus' : 'Defecit')} 250
+                                <br />
+                                Daily Calories needed: {(Calculate() + (item.todo === 'Gain' ? 250 : -250))}
                                 <br />
 
-                                This is based on a 500 calorie per day ?
-                                <br />
-                                Days to goal with  500 calorie ?: {item.weight * 3500 / 500}
                             </Typography>
                         </CardContent>
                         <CardActions disableSpacing>
                             <Typography variant="body2" color="text.secondary">
-                                Calorie to target per day...
-                                NEED {(Calculate() + (item.todo === 'Gain' ? 500 : -500))}
+                                click here for more options
 
                             </Typography>
-                            <IconButton aria-label="">
+                            <IconButton aria-label="" sx={{
+                                position: 'relative',
+                                top: '30px',
+                                left: '140px',
+                            }} >
                                 <DeleteIcon onClick={() => {
                                     console.log('item.id:', item.id);
                                     dispatch({ type: 'DELETE_SIM_ITEM', payload: item.id })
                                 }} />
-                            </IconButton>
+                            </IconButton >
                             <ExpandMore
                                 expand={expanded}
                                 onClick={handleExpandClick}
@@ -167,7 +150,29 @@ function SimulatorTable() {
                         </CardActions>
                         <Collapse in={expanded} timeout="auto" unmountOnExit>
                             <CardContent>
-                                <Typography paragraph>i can change this:</Typography>
+                                <Typography variant="body2" color="text.secondary" fontSize='18px'>
+                                    Approach: moderate
+                                    <br />
+                                    Days: {item.weight * 3500 / 500}
+                                    <br />
+                                    Calorie: {(item.todo === 'Gain' ? 'Surplus' : 'Defecit')} 500
+                                    <br />
+                                    Daily Calories needed: {(Calculate() + (item.todo === 'Gain' ? 500 : -500))}
+                                    <br />
+
+                                </Typography>
+                                <br />
+                                <Typography variant="body2" color="text.secondary" fontSize='18px'>
+                                    Approach: Aggressive
+                                    <br />
+                                    Days: {item.weight * 3500 / 1000}
+                                    <br />
+                                    Calorie: {(item.todo === 'Gain' ? 'Surplus' : 'Defecit')} 1000
+                                    <br />
+                                    Daily Calories needed: {(Calculate() + (item.todo === 'Gain' ? 1000 : -1000))}
+                                    <br />
+
+                                </Typography>
 
                             </CardContent>
                         </Collapse>
